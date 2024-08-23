9ceddbb92b5f355b6b25873e18255d5fba298744
Ashmita246 <ashmitachimouriya123@gmail.com>
2024-08-23 09:38:06 +0545

    animated letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/download-removebg-preview.png'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
 
 
 
 const Home = ()=>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['s','h','m','i','t','a']
+    const jobArray= ['w','e','b',' ','d','e','v','e','l','o','p','e','r']
     return(
 <div className="container home-page">
     <div className="text-zone">
-        <h1>Hi, <br/>I'm  
+        <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+             <br/>
+             <span className={`${letterClass} _13`}>I</span>
+             <span className={`${letterClass} _14`}>'m</span> 
         <img src ={LogoTitle}alt="developer"/>
-        shmita
+        <AnimatedLetters letterClass={letterClass}
+        strArray={nameArray}
+        idx={15}/>
         <br/>
-        web developer
+        <AnimatedLetters letterClass={letterClass}
+        strArray={jobArray}
+        idx={22}/>
         </h1>
         <h2>Backend Developer/Node js</h2>
         <Link to="./contact" className="flat-button">CONTACT ME</Link>
